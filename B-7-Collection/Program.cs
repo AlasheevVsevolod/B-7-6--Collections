25160e6f72c5f49cc072a28bd07862f116e2360a
Vsevolod <2007juggernaut@gmail.com>
2019-01-02 14:07:50 +0300

    task1

--- B-7-Collection/Program.cs
+++ B-7-Collection/Program.cs
@@ -7,88 +7,79 @@ using System.Threading.Tasks;
 
 namespace Base.Lesson_5
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //ArrayListExample();
-
-            ArrayListPoem();
-
-            Console.ReadLine();
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//B7-P1/5. ArrayListPoemSort.
+			B7_P1();
 
 
-        public static void ArrayListPoem()
-        {
-            ArrayList newArrList = new ArrayList();
-            for (int i = 0; i < 3; i++)
-            {
-                newArrList.Add(Console.ReadLine());
-
-                var Songs = new Song()
-                {
-                    Lyrics = Console.ReadLine()
-                };
-            }
-
-            PrintArrList(newArrList);
-
-            newArrList.Sort();
-            PrintArrList(newArrList);
-
-            newArrList.RemoveAt(newArrList.Count - 1);
-            PrintArrList(newArrList);
+			Console.ReadLine();
+		}
 
 
-/*            foreach (var row in Songs)
-            {
+		//B7-P1/5. ArrayListPoemSort.
+		public static void B7_P1()
+		{
+			ArrayList newArrList = new ArrayList();
+			for (int i = 0; i < 3; i++)
+			{
+				newArrList.Add(Console.ReadLine());
+			}
+			Console.WriteLine();
 
-            }
-*/
-    }
+			PrintArrList(newArrList);
 
-        public static void PrintArrList(ArrayList tmpArr)
-        {
-            foreach (string listString in tmpArr)
-            {
-                Console.WriteLine(listString);
-            }
-            Console.WriteLine();
-        }
+			newArrList.Sort();
+			PrintArrList(newArrList);
+
+			newArrList.RemoveAt(newArrList.Count - 1);
+			PrintArrList(newArrList);
+		}
+
+		public static void PrintArrList(ArrayList tmpArr)
+		{
+			foreach (string listString in tmpArr)
+			{
+				Console.WriteLine(listString);
+			}
+			Console.WriteLine();
+		}
 
 
 
-        public static void ArrayListExample()
-        {
-            var poem = new ArrayList();
-            for (int i = 0; i < 5; i++)
-            {
-                var song = new Song();
-                song.Lyrics = Console.ReadLine();
-                poem.Add(song);
-            }
+/*		public static void ArrayListExample()
+		{
+			var poem = new ArrayList();
+			for (int i = 0; i < 5; i++)
+			{
+				var song = new Song();
+				song.Lyrics = Console.ReadLine();
+				poem.Add(song);
+			}
 
-            //poem.Sort();
-            poem.RemoveAt(poem.Count - 1);
+			//poem.Sort();
+			poem.RemoveAt(poem.Count - 1);
 
-            object[] myArray = poem.ToArray();
+			object[] myArray = poem.ToArray();
 
-            foreach (var item in myArray)
-            {
-                Console.WriteLine(item);
-            }
+			foreach (var item in myArray)
+			{
+				Console.WriteLine(item);
+			}
 
-        }
+		}
 
-        public class Song : IComparable
-        {
-            public string Lyrics { get; set; }
+		public class Song : IComparable
+		{
+			public string Lyrics { get; set; }
 
-            public int CompareTo(object obj)
-            {
-                throw new NotImplementedException();
-            }
-        }
-    }
+			public int CompareTo(object obj)
+			{
+				throw new NotImplementedException();
+			}
+		}
+		*/
+	}
 }
